1cc311aa2d6ef058f1d8da956ef06ea06c29feb1
rqure <64617806+rqure@users.noreply.github.com>
2024-01-27 14:07:06 -0700

    Create main.go

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"strconv"
+	"time"
+
+	qmq "github.com/rqure/qmq/src"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func main() {
+	app := qmq.NewQMQApplication("audio-player")
+	app.Initialize()
+	defer app.Deinitialize()
+
+	app.AddConsumer("audio-player:queue")
+
+	tickRateMs, err := strconv.Atoi(os.Getenv("TICK_RATE_MS"))
+	if err != nil {
+		tickRateMs = 100
+	}
+
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, os.Interrupt)
+
+	ticker := time.NewTicker(time.Duration(tickRateMs) * time.Millisecond)
+  defer ticker.Close()
+	for {
+		select {
+		case <-sigint:
+			app.Logger().Advise("SIGINT received")
+			return
+		case <-ticker.C:
+      
+		}
+	}
+}

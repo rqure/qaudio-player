80871e422992a6b9f523266f9b741a40f7bf7e95
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2024-06-27 22:23:41 -0600

    some progress

--- audio_player.go
+++ audio_player.go
@@ -1,5 +1,52 @@
 package main
 
-type AudioPlayer interface {
-	Play(filename string) error
+import (
+	"context"
+	"os/exec"
+
+	qdb "github.com/rqure/qdb/src"
+)
+
+type IAudioPlayer interface {
+	Play(filename string)
+	IsPlaying() bool
+	Cancel()
+}
+
+type AudioPlayer struct {
+	cancel context.CancelFunc
+}
+
+func NewAudioPlayer() IAudioPlayer {
+	return &AudioPlayer{
+		cancel: nil,
+	}
+}
+
+func (a *AudioPlayer) Play(filename string) {
+	ctx, cancel := context.WithCancel(context.Background())
+	a.cancel = cancel
+
+	cmd := exec.CommandContext(ctx, "play", filename)
+
+	go func() {
+		if err := cmd.Run(); err != nil {
+			qdb.Error("[AudioPlayer::Play] Failed to play audio")
+		}
+
+		a.cancel = nil
+	}()
+}
+
+func (a *AudioPlayer) IsPlaying() bool {
+	return a.cancel != nil
+}
+
+func (a *AudioPlayer) Cancel() {
+	if a.cancel == nil {
+		return
+	}
+
+	a.cancel()
+	a.cancel = nil
 }
